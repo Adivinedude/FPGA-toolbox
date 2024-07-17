2fe02ee07be498b42f1e45a2f3a1638d48405f31
Ronald Rainwater <adivinedude@hotmail.com>
2024-07-17 11:18:08 -0400

    mux_pipeline debug complete

--- mux_pipeline.v
+++ mux_pipeline.v
@@ -35,7 +35,8 @@ module mux_pipeline #(
     parameter WIDTH = 1,
     parameter INPUT_COUNT = 2,
     parameter LATENCY = 0,
-    parameter TYPE = 0
+    parameter TYPE = 0,
+    parameter PRINT = 0
 )( clk, sel, in, out );
     input   wire                                clk;
     input   wire    [$clog2(INPUT_COUNT)-1:0]   sel;
@@ -53,8 +54,9 @@ module mux_pipeline #(
             case(TYPE)
                 default:    f_GetMuxSize = f_NaryRecursionGetUnitWidthForLatency(INPUT_COUNT, LATENCY);
             endcase
-            $display("f_GetMuxSize: L:%1d\tIC:%1d \t%1d\t%1d",LATENCY, INPUT_COUNT, f_GetMuxSize, 'd1 << $clog2(f_GetMuxSize));
+            if(PRINT!=0)$write("f_GetMuxSize: LATENCY:%0d\tINPUT_COUNT:%0d \f_NaryRecursionGetUnitWidthForLatency:%0d\t",LATENCY, INPUT_COUNT, f_GetMuxSize );
             f_GetMuxSize = 'd1 << $clog2(f_GetMuxSize);
+            if(PRINT!=0)$display("f_GetMuxSize:%0d", f_GetMuxSize);
         end
     endfunction
     localparam MUX_SIZE = f_GetMuxSize(0);
@@ -73,7 +75,7 @@ module mux_pipeline #(
     reg     [(STRUCTURE_SIZE*WIDTH)-1:0]    r_in_pipeline;
     wire    [(STRUCTURE_SIZE*WIDTH)-1:0]    w_out_pipeline;
 
-    mux_combinational #(.WIDTH(WIDTH), .INPUT_COUNT(INPUT_COUNT), .LATENCY(LATENCY), .TYPE(TYPE) )
+    mux_combinational #(.WIDTH(WIDTH), .INPUT_COUNT(INPUT_COUNT), .LATENCY(LATENCY), .TYPE(TYPE), .PRINT(PRINT) )
         mux_object(.clk(clk), .sel(sel), .in(in), .in_pipeline(r_in_pipeline), .out(out), .out_pipeline(w_out_pipeline) );
     
     always @( posedge clk ) r_in_pipeline <= w_out_pipeline;
@@ -83,9 +85,10 @@ module mux_combinational #(
     parameter WIDTH = 1,
     parameter INPUT_COUNT = 2,
     parameter LATENCY = 0, 
-    parameter TYPE = 0      // 0 - Fixed latency for all selections
+    parameter TYPE = 0,     // 0 - Fixed latency for all selections
                             // 1 - Optimized structure - possible variable latency for a given selection
                             // 2 - Prioritized structure - MSB selection will have smallest latency.
+    parameter PRINT = 0
 )( clk, sel, in, in_pipeline, out, out_pipeline );
     input   wire                                clk;
     input   wire    [$clog2(INPUT_COUNT)-1:0]   sel;
@@ -125,11 +128,11 @@ module mux_combinational #(
             case(TYPE)
                 default:    f_GetMuxSize = f_NaryRecursionGetUnitWidthForLatency(INPUT_COUNT, LATENCY);
             endcase
-            // $display("f_GetMuxSize: L:%1d\tIC:%1d \t%1d\t%1d",LATENCY, INPUT_COUNT, f_GetMuxSize, 'd1 << $clog2(f_GetMuxSize));
             f_GetMuxSize = 'd1 << $clog2(f_GetMuxSize);
         end
     endfunction
     localparam MUX_SIZE = f_GetMuxSize(0);
+
     function automatic integer f_GetVectorSize;
         input unused;
         begin
@@ -181,82 +184,43 @@ module mux_combinational #(
     localparam STRUCTURE_SIZE = f_GetVectorSize(0);
     localparam STRUCTURE_DEPTH = f_GetStructureDepth(0);
     localparam SEL_WIDTH = $clog2(MUX_SIZE);
-    initial $display("INPUT_COUNT:%0d MUX_SIZE:%0d STRUCTURE_SIZE:%0d STRUCTURE_DEPTH:%0d SEL_WIDTH:%0d", INPUT_COUNT, MUX_SIZE, STRUCTURE_SIZE, STRUCTURE_DEPTH, SEL_WIDTH);
+    localparam TOTAL_UNIT_COUNT = f_NaryRecursionGetVectorSize(INPUT_COUNT,MUX_SIZE);
     
     input   wire    [(STRUCTURE_SIZE*WIDTH)-1:0]        in_pipeline;
     output  wire    [(STRUCTURE_SIZE*WIDTH)-1:0]        out_pipeline;
     wire    [((INPUT_COUNT+STRUCTURE_SIZE-1)*WIDTH)-1:0]  w_input_chain;
     assign w_input_chain = {in_pipeline,in};
     generate
+        initial if(PRINT!=0)$display("INPUT_COUNT:%0d MUX_SIZE:%0d STRUCTURE_TYPE:%0d STRUCTURE_SIZE:%0d STRUCTURE_DEPTH:%0d SEL_WIDTH:%0d", INPUT_COUNT, MUX_SIZE, TYPE, STRUCTURE_SIZE, STRUCTURE_DEPTH, SEL_WIDTH);
         genvar unit_index, input_index;
-        for( unit_index = 0; unit_index < STRUCTURE_SIZE; unit_index = unit_index + 1) begin : mux_unit_loop
-        // for( unit_index = 0; unit_index < 1; unit_index = unit_index + 1) begin : mux_unit_loop
-            initial $write(" unit_index:%2d output_index:%2d unit_width:%2d unit_depth:%2d", unit_index, f_GetUnitOutputAddress(unit_index)!=~0?f_GetUnitOutputAddress(unit_index):~0, f_GetUnitWidth(unit_index), f_GetUnitDepth(unit_index));
+        for( unit_index = 0; unit_index < TOTAL_UNIT_COUNT; unit_index = unit_index + 1) begin : mux_unit_loop
             if( f_GetUnitOutputAddress(unit_index) != ~0 ) begin
                 // build the unit's inputs.
-                wire [STRUCTURE_SIZE*WIDTH-1:0] unit_inputs;
-                for( input_index = 0; input_index < STRUCTURE_SIZE; input_index = input_index + 1 ) begin : mux_input_loop
+                wire [MUX_SIZE*WIDTH-1:0] unit_inputs;
+                for( input_index = 0; input_index < MUX_SIZE; input_index = input_index + 1 ) begin : mux_input_loop
                     if( input_index < f_GetUnitWidth(unit_index) ) begin
-                        initial $write(" (II:%2d A:%2d)", input_index, f_GetInputAddress(unit_index, input_index) );
+                        initial if(PRINT!=0)$write(" (II:%2d A:%2d)", input_index, f_GetInputAddress(unit_index, input_index) );
                         assign unit_inputs[WIDTH*input_index+:WIDTH] = w_input_chain[f_GetInputAddress(unit_index, input_index)*WIDTH+:WIDTH];
                     end else 
                         assign unit_inputs[WIDTH*input_index+:WIDTH] = {WIDTH{1'bz}};
                 end
                 // select the units output.
                 if( f_GetUnitWidth(unit_index) != 1 ) begin
-                    initial $display(" sel - SEL[%0d+:%0d]", f_GetUnitDepth(unit_index)*SEL_WIDTH, SEL_WIDTH );
+                    initial if(PRINT!=0)$display(" sel - SEL[%0d+:%0d]", f_GetUnitDepth(unit_index)*SEL_WIDTH, SEL_WIDTH );
                     assign out_pipeline[(f_GetUnitOutputAddress(unit_index))*WIDTH+:WIDTH] = unit_inputs[sel[f_GetUnitDepth(unit_index)*SEL_WIDTH+:SEL_WIDTH]*WIDTH+:WIDTH];
                 end else begin
-                    initial $display(" set");
-                    assign out_pipeline[(f_GetUnitOutputAddress(unit_index))*WIDTH+:WIDTH] = unit_inputs[sel[0+:SEL_WIDTH]*WIDTH+:WIDTH];
+                    initial if(PRINT!=0)$display(" set");
+                    assign out_pipeline[(f_GetUnitOutputAddress(unit_index))*WIDTH+:WIDTH] = unit_inputs[0+:WIDTH];
                 end
-                if( unit_index == STRUCTURE_SIZE-1 ) begin
-                    initial $display("OUT = UI:%0d", unit_index);
+                if( unit_index == f_NaryRecursionGetVectorSize(INPUT_COUNT,MUX_SIZE)-1 ) begin
+                    initial if(PRINT!=0)$display("OUT = UI:%0d", unit_index);
                     // always @(posedge clk) $display("OUT:%0d UI:%0d SEL:%0d", out, unit_index, sel);
                     assign out = unit_inputs[ sel[f_GetUnitDepth(unit_index)*SEL_WIDTH+:SEL_WIDTH]*WIDTH +:WIDTH ];
                 end
+            end else begin
+                initial if(PRINT!=0)$display("");
             end
+            initial if(PRINT!=0)$write(" unit_index:%2d output_index:%2d unit_width:%2d unit_depth:%2d", unit_index, f_GetUnitOutputAddress(unit_index)!=~0?f_GetUnitOutputAddress(unit_index)+INPUT_COUNT:~0, f_GetUnitWidth(unit_index), f_GetUnitDepth(unit_index));
         end
     endgenerate
-endmodule
-
-
-module mux_tb;
-    reg             clk = 0;
-    reg     [39:0]   in;
-    integer in_loop;
-    initial begin
-        for(in_loop = 0; in_loop < 10; in_loop = in_loop + 1 )
-            in[in_loop*4+:4] = in_loop[3:0];
-    end
-
-    wire    [3:0]   out;
-    reg     [7:0]   sel = 0;
-
-    mux_pipeline#(.WIDTH(4), .INPUT_COUNT(10), .LATENCY(2), .TYPE(1) )
-        UUT( .clk(clk), .sel(sel[7:4]), .in(in), .out(out) );
-    
-    always #1 clk <= ~clk;
-    always @(posedge clk) begin
-        sel <= sel + 1'b1;
-        // $display("sel:%d\tout:%d", sel[5:4], out);
-    end
-
-    reg [1:0] correct = 0;
-    always @( posedge clk ) begin
-        if( sel[3:0] == out )
-            correct <= { correct[0], 1'b1 };
-        else
-            correct <= 0;
-        if( correct == {1'b0, 1'b1} )
-            $display( "time:%0d sel:%0d out:%0d", $time, sel[7:4], out );
-    end
-
-    initial begin
-        $dumpfile("UUT.vcd");
-        $dumpvars(0, mux_tb);
-        $display("starting mux_tb.v");
-        #600 $display( "***WARNING*** Forcing simulation to end");
-        $finish;
-    end
 endmodule
