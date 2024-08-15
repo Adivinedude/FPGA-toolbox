1e83b573b6db993c29fb271206ed722569ec2696
Ronald Rainwater <adivinedude@hotmail.com>
2024-08-14 21:35:27 -0400

    pipeline_vector implmented

--- pipeline_vector.v
+++ pipeline_vector.v
@@ -4,7 +4,7 @@
 //
 // Project:	pipeline vector logic
 //
-// Purpose:	a sequential vector pipeline data structures.
+// Purpose:	a purely combinational sequential vector pipeline data structures.
 //
 // Creator:	Ronald Rainwater
 // Data: 2024-8-14
@@ -29,27 +29,89 @@
 
 ////////////////////////////////////
 // pipeline vector. diagram
-// <2> <1> <0>
-//     <4> <3>
-//         <5>
-// 
+// <MSB --> LSB>
+//  step 1        |  step 2        |  step 3
+// out_shift_left
+// <A2> <A1> <A0> | <B2> <B1> <B0> | <C2> <C1> <C0>
+// <XX> <XX>      | <A1> <A0>      | <B1> <B0>
+// <XX>           | <XX>           | <A0>
+// sel_left
+// <XX> <XX> <A2> | <XX> <A1> <B2> | <A0> <B1> <C2>
+
+// out_shift_right
+// <A2> <A1> <A0> | <B2> <B1> <B0> | <C2> <C1> <C0>
+//      <XX> <XX> |      <A2> <A1> |      <B2> <B1>
+//           <XX> |           <XX> |           <A2>
+// sel_right
+// <XX> <XX> <A0> | <XX> <A1> <B0> | <A2> <B1> <C0>
+
 
 `default_nettype none
 
 module pipeline_vector #( 
     parameter WIDTH = 1,
-    parameter SIZE  = 2
-    )( clk, in, out_shift_left, out_shift_right, sel_left, sel_right );
+    parameter SIZE  = 3,
+    parameter PRINT = 1
+    )( in, out_shift_left, out_shift_right, sel_left, sel_right );
 
     `include "recursion_iterators.vh"
 
-    localparam VECTOR_SIZE = f_GetPipelineVectorSize( SIZE, WIDTH );
+    localparam VECTOR_SIZE = f_GetPipelineVectorSize( SIZE - 1, WIDTH );
 
-    input   wire                        clk;
-    input   wire    [VECTOR_SIZE-1:0]   in;
+    input   wire    [VECTOR_SIZE+(SIZE*WIDTH)-1:0]   in;
     output  wire    [VECTOR_SIZE-1:0]   out_shift_left;
     output  wire    [VECTOR_SIZE-1:0]   out_shift_right;
     output  wire    [SIZE*WIDTH -1:0]   sel_left;
     output  wire    [SIZE*WIDTH -1:0]   sel_right;
+    // out_shift_left
+    for( idx = 0; idx < SIZE-1; idx = idx + 1 )begin
+        if(PRINT!=0)initial $display( "pipeline_vector - idx:%1d out_shift_left[%1d:%1d] = in[%1d:%1d]", 
+            idx,
+            f_GetPipelineDepthEndAddress(SIZE-1, WIDTH, idx),
+            f_GetPipelineDepthStartAddress(SIZE-1, WIDTH, idx),
+            f_GetPipelineDepthEndAddress(SIZE, WIDTH, idx)-WIDTH,
+            f_GetPipelineDepthStartAddress(SIZE, WIDTH, idx)
+        );
+        assign out_shift_left[   f_GetPipelineDepthEndAddress(SIZE-1, WIDTH, idx)
+                                :f_GetPipelineDepthStartAddress(SIZE-1, WIDTH, idx) ]
+                = in[    f_GetPipelineDepthEndAddress(SIZE, WIDTH, idx)-WIDTH
+                        :f_GetPipelineDepthStartAddress(SIZE, WIDTH, idx)];
+    end
+    // out_shift_right
+    for( idx = 0; idx < SIZE-1; idx = idx + 1 )begin
+        if(PRINT!=0)initial $display( "pipeline_vector - idx:%1d out_shift_right[%1d:%1d] = in[%1d:%1d]", 
+            idx,
+            f_GetPipelineDepthEndAddress(SIZE-1, WIDTH, idx),
+            f_GetPipelineDepthStartAddress(SIZE-1, WIDTH, idx),
+            f_GetPipelineDepthEndAddress(SIZE, WIDTH, idx),
+            f_GetPipelineDepthStartAddress(SIZE, WIDTH, idx)+WIDTH
+        );
+        assign out_shift_right[  f_GetPipelineDepthEndAddress(SIZE-1, WIDTH, idx)
+                                :f_GetPipelineDepthStartAddress(SIZE-1, WIDTH, idx) ]
+                = in[    f_GetPipelineDepthEndAddress(SIZE, WIDTH, idx)
+                        :f_GetPipelineDepthStartAddress(SIZE, WIDTH, idx)+WIDTH];
+    end
+    // sel_left
+    for( idx = 0; idx < SIZE; idx = idx + 1 )begin
+        if(PRINT!=0)initial $display( "pipeline_vector - idx:%1d sel_left[%1d+:%1d] = in[%1d+:%1d]", 
+            idx,
+            idx*WIDTH,
+            WIDTH,
+            f_GetPipelineDepthEndAddress(SIZE, WIDTH, idx),
+            WIDTH
+        );
+        assign sel_left[ idx*WIDTH+:WIDTH ] = in[f_GetPipelineDepthEndAddress(SIZE, WIDTH, idx) +: WIDTH];
+    end
+    // sel_right
+    for( idx = 0; idx < SIZE; idx = idx + 1 )begin
+        if(PRINT!=0)initial $display( "pipeline_vector - idx:%1d sel_right[%1d+:%1d] = in[%1d+:%1d]", 
+            idx,
+            idx*WIDTH,
+            WIDTH,
+            f_GetPipelineDepthStartAddress(SIZE, WIDTH, idx),
+            WIDTH
+        );
+        assign sel_left[ idx*WIDTH+:WIDTH ] = in[f_GetPipelineDepthStartAddress(SIZE, WIDTH, idx) +: WIDTH];
+    end
 
 endmodule
